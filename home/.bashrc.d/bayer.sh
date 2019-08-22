25eb143288459165d0d0af3ebbe34aea6340952f
Doug Schott <douglas.schott@monsanto.com>
2019-08-22 15:44:03 -0400

    add bayer vpn scripts

--- home/.bashrc.d/bayer.sh
+++ home/.bashrc.d/bayer.sh
@@ -2,24 +2,28 @@
 
 [ "$(hostname)" != "C02XP0MFJGH5" ] && return
 
-export USER_FULLNAME='Doug Schott'
-export USER_EMAIL='douglas.schott@monsanto.com'
+export USER_FULLNAME="Doug Schott"
+export USER_EMAIL="douglas.schott@monsanto.com"
 export VAULT_ADDR=https://vault.agro.services
 
-vault-login() {
+vault-login-bayer() {
     vault login -method=userpass -path=ping username=GKRPS
 }
 
-proxy-off() {
-    networksetup -setautoproxystate Wi-Fi off
+vpn-connect-bayer() {
+    vpn-connect "Standard VPN - IRV"
+    proxy-off
 }
 
-proxy-state() {
-    networksetup -getautoproxyurl Wi-Fi
+vpn-disconnect-bayer() {
+    vpn-disconnect
+    proxy-off
 }
 
-proxy-on() {
-    networksetup -setautoproxystate Wi-Fi on
+open-bayernet() {
+    vpn-disconnect
+    proxy-on
+    proxy-seturl "http://pac.hybrid-web.global.blackspider.com/proxy.pac?p=4m92bzff"
+    vpn-connect "Standard VPN - IRV"
+    open "https://new.bayernet.cnb/en"
 }
-
-proxy-off
