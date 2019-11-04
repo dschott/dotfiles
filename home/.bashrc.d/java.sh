a91fbb2d4e2ee5ad94adb85e410b20c2c1c2b1cb
Doug Schott <douglas.schott@gmail.com>
2019-11-03 21:00:21 -0500

    java changes

--- home/.bashrc.d/java.sh
+++ home/.bashrc.d/java.sh
@@ -24,38 +24,11 @@ java-version() {
 
     if os-is-darwin; then
         export JAVA_HOME="$(/usr/libexec/java_home -v ${1})"
+    elif os-is-linux; then
+        sudo update-java-alternatives -s "${1}"
     fi
 }
 
-java-version 12
-
-
-# if os-is-darwin; then
-#     # Ref: http://www.jayway.com/2014/01/15/how-to-switch-jdk-version-on-mac-os-x-maverick/
-#     if [ -z "$1" ]; then
-#         printf "Current Version:\n    ${current_version}\n\nAvailable Versions:\n$(/usr/libexec/java_home -V 2>&1 | grep Library)\n"
-#     elif . sh-is-subshell; then
-#         echo "$0 must be sourced"
-#     else
-#         . path-remove '/System/Library/Frameworks/JavaVM.framework/Home/bin'
-
-#         if [ -n "${JAVA_HOME+x}" ]; then
-#             . path-remove ${JAVA_HOME}
-#         fi
-
-#         export JAVA_HOME=`/usr/libexec/java_home -v $@`
-# # alias j8="export JAVA_HOME=`/usr/libexec/java_home -v 1.8`; java -version"
-
-#         . path-add "${JAVA_HOME}/bin"
-#     fi
-# elif os-is-debian; then
-#     if [ -z "$1" ]; then
-#         echo "current version:"
-#         echo "    ${current_version}"
-#         echo
-#         echo "available versions:"
-#         update-java-alternatives -l | sed 's/^/    /'
-#     else
-#         sudo update-java-alternatives -s "${1}"
-#     fi
-# fi
+if os-is-darwin; then
+    java-version 12
+fi
