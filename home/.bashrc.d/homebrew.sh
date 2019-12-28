d1874e4f8fd9d432bcdbf9c230e6fb701bd6dd71
Schott, Douglas <douglas.schott@gmail.com>
2019-12-28 12:13:05 -0500

    begin migration from snap to homebrew

--- home/.bashrc.d/homebrew.sh
+++ home/.bashrc.d/homebrew.sh
@@ -2,13 +2,18 @@
 
 if check-command -q brew; then
   HOMEBREW_PREFIX=$(brew --prefix)
-  if type brew &>/dev/null; then
-    if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]; then
-      . "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
-    else
-      for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*; do
-        [[ -r "${COMPLETION}" ]] && . "${COMPLETION}"
-      done
-    fi
+  if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]; then
+    . "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
+  else
+    for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*; do
+      [[ -r "${COMPLETION}" ]] && . "${COMPLETION}"
+    done
   fi
+
+case "$(uname -s)" in
+Linux)
+    path-add "$(brew --prefix)/bin"
+    ;;
+esac
+
 fi
