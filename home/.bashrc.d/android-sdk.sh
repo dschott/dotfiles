5b0a31175293be18b52fc3869779512b83aaff0e
Schott, Douglas <douglas.schott@gmail.com>
2021-03-21 16:09:50 -0400

    fixup android sdk install and bashrc

--- home/.bashrc.d/android-sdk.sh
+++ home/.bashrc.d/android-sdk.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-if [[ -d ${HOME}/android ]]; then
+if [[ -d ${HOME}//Android-Sdk ]]; then
     export ANDROID_HOME=${HOME}/Android-Sdk
-    path-add "$ANDROID_HOME/cmdline-tools/latest/bin"
-    path-add "$ANDROID_HOME/platform-tools"
+    path-add "${ANDROID_HOME}/cmdline-tools/latest/bin"
+    path-add "${ANDROID_HOME}/tools"
+    path-add "${ANDROID_HOME}/tools/bin"
+    path-add "${ANDROID_HOME}/platform-tools"
 fi
