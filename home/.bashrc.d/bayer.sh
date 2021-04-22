7c9cc9f1408dd53fd0a985f942ccbf7a7c487592
Schott, Douglas <douglas.schott.ext@bayer.com>
2021-04-22 11:07:53 -0400

    Add bayer aws config

--- home/.bashrc.d/bayer.sh
+++ home/.bashrc.d/bayer.sh
@@ -1,27 +1,34 @@
 #!/usr/bin/env bash
 
-vault-login-bayer() {
-    vault login -method=oidc
-}
+if [[ $USER == gkrps ]]; then
 
-vpn-connect-bayer() {
-    vpn-connect "Standard VPN - IRV"
-    proxy-off
-}
+    vault-login-bayer() {
+        vault login -method=oidc
+    }
 
-vpn-disconnect-bayer() {
-    vpn-disconnect
-    proxy-off
-}
+    vpn-connect-bayer() {
+        vpn-connect "Standard VPN - IRV"
+        proxy-off
+    }
 
-proxy-on-bayer() {
-    proxy-on
-    proxy-seturl "http://pac.hybrid-web.global.blackspider.com/proxy.pac?p=4m92bzff"
-}
+    vpn-disconnect-bayer() {
+        vpn-disconnect
+        proxy-off
+    }
 
-bayernet-open() {
-    vpn-disconnect
-    proxy-on-bayer
-    vpn-connect "Standard VPN - IRV"
-    open "https://new.bayernet.cnb/en"
-}
+    proxy-on-bayer() {
+        proxy-on
+        proxy-seturl "http://pac.hybrid-web.global.blackspider.com/proxy.pac?p=4m92bzff"
+    }
+
+    bayernet-open() {
+        vpn-disconnect
+        proxy-on-bayer
+        vpn-connect "Standard VPN - IRV"
+        open "https://new.bayernet.cnb/en"
+    }
+
+    export AWS_PROFILE=saml
+    alias aws-sso='/Users/gkrps/Library/Python/3.9/bin/aws-sso -u douglas.schott.ext@bayer.com'
+
+fi
