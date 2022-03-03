6ab0c7bdcc15da5ffc9f9025afb66f51e5f47108
Schott, Douglas <douglas.schott@gmail.com>
2022-03-03 16:56:16 -0500

    Fix compose alias and completion

--- home/.bashrc.d/docker.sh
+++ home/.bashrc.d/docker.sh
@@ -2,10 +2,5 @@
 
 if check-command -q docker; then
     alias d='docker'
-    complete -F _docker d
-fi
-
-if check-command -q docker-compose; then
-    alias dc='docker-compose'
-    complete -F _docker_compose dc
+    alias dc='docker compose'
 fi
