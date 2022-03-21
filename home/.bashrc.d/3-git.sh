6689ee8033f57cfdd397eca7db151fdb20748af6
Schott, Douglas <douglas.schott@gmail.com>
2022-03-21 17:36:07 -0400

    Fix paging

--- home/.bashrc.d/3-git.sh
+++ home/.bashrc.d/3-git.sh
@@ -3,5 +3,6 @@
 if check-command -q git; then
     alias g='git'
     complete -F _complete_alias g
-    export DELTA_PAGER="less -+FX -R --tabs 4 -c"
+    export DELTA_PAGER="less -R --tabs 4"
+    export DELTA_PAGER_PAGED="less -+FX -R --tabs 4 -c"
 fi
